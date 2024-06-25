0d2259c7bd8fc484df033854b4a3eb10ffb02209
Ashley Meza <ashley.meza-medina@csedge.org>
2024-06-25 13:35:26 -0700

    Created MadLib V1

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,25 @@
 # MadLib by Ashley
 
 print("Here is my first commit :) :) :) :0")
+
+adverb_1 = input("Enter an adverb: ")
+noun_1 = input("Enter a noun: ")
+adjective_1 = input("Enter an adjective: ")
+verb_1 = input("Enter a verb: ")
+noun_2 = input("Enter a noun: ")
+noun_3 = input("Enter a noun: ")
+place_1 = input("Enter your hometown: ")
+name_1 = input("Enter a name: ")
+verb_2 = input("Enter a verb: ")
+verb_3 = input("Enter a verb: ")
+noun_4 = input("Enter a noun: ")
+
+
+
+
+
+
+# print("You are " + adjective_1)
+print(f"It is a Tuesday morning in {place_1}, and you are {verb_1}.")
+print(f"In attempts to {adverb_1} go over to see {noun_2}, you and {name_1} go to {verb_2}") 
+print(f"At the sight of {noun_2}, there came a {noun_3}, {adjective_1} {noun_4} {verb_3} ready to take on the ship to outerspace.")
